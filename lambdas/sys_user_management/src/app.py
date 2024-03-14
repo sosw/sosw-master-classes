df1e092307088980393597ac4d85a22ec86b3a1c
kreanimator <backin.v@yandex.ru>
2024-03-14 14:30:14 +0200

    Fix #3

--- lambdas/sys_user_management/src/app.py
+++ lambdas/sys_user_management/src/app.py
@@ -24,37 +24,28 @@ class Processor(SoswProcessor):
     def __call__(self, event):
 
         logger.info("Called the Processor")
-        try:
-            self.sys_user_management(event)
-        except RuntimeError as err:
-            logger.exception(err)
-            self.stats['failed_events'] += 1
+        self.sys_user_management(event)
 
 
     def sys_user_management(self, event):
-        # Assuming 'names' key contains the list of usernames
+
         names = event.get('names', [])
 
         users = {}
 
-        # Generating password to users
         for name in names:
             password = self.generate_random_password()
-            try:
-                # Creating users and attaching policies to them
-                self.iam_client.create_user(UserName=name)
-                self.iam_client.create_login_profile(UserName=name, Password=password, PasswordResetRequired=False)
-                self.iam_client.attach_user_policy(UserName=name, PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess')
-                users[name] = password
-            except Exception as e:
-                logger.critical("Error creating user", name, e)
+
+            self.iam_client.create_user(UserName=name)
+            self.iam_client.create_login_profile(UserName=name, Password=password, PasswordResetRequired=False)
+            self.iam_client.attach_user_policy(UserName=name, PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess')
+            users[name] = password
 
         logger.info(f"Created users: {json.dumps(users)}")
         return users
 
 
-    def generate_random_password(self, length=12):
-        # Generating random password
+    def generate_random_password(self, length: int = 8):
         characters = string.ascii_letters + string.digits + string.punctuation
         return ''.join(random.choice(characters) for _ in range(length))
 
