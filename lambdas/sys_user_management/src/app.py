1e5eb62eee2da1d639ed191ffcbfb26019884bf0
kreanimator <backin.v@yandex.ru>
2024-03-17 17:20:09 +0200

    Fixed loggers, added uppercase to password generation, refactored code

--- lambdas/sys_user_management/src/app.py
+++ lambdas/sys_user_management/src/app.py
@@ -12,8 +12,9 @@ logger = Logger()
 class Processor(SoswProcessor):
     DEFAULT_CONFIG = {
         'init_clients':                ['iam', 'lambda'],
-        'cleanup_exclude_list_lambda': [],
-        'cleanup_exclude_list_role':   ['AWSServiceRoleForComputeOptimizer ',
+        'cleanup_exclude_list_lambda': ['sys_user_management'],
+        'cleanup_exclude_list_role':   ['AWSServiceRoleForComputeOptimizer',
+                                        'AWSServiceRoleForComputeOptimizer',
                                         'AWSServiceRoleForOrganizations',
                                         'AWSServiceRoleForSupport',
                                         'AWSServiceRoleForTrustedAdvisor',
@@ -32,22 +33,22 @@ class Processor(SoswProcessor):
 
         logger.info("Called the Processor")
 
-        trigger = event.get('action', [])
+        trigger = event.get('action', '')
 
         match trigger:
             case ['create']:
-                self.sys_user_management(event)
                 logger.info('Creating users')
+                self.sys_user_management_create_users(event)
 
             case ['delete']:
+                logger.info('Initializing deletion of users, roles, lambdas and policies.')
                 self.sys_user_management_cleanup()
-                logger.info('Initialize deletion of users, roles, lambdas and policies.')
 
             case _:
                 logger.info('Action is not supported')
 
 
-    def sys_user_management(self, event):
+    def sys_user_management_create_users(self, event):
 
         names = event.get('names', [])
 
@@ -77,7 +78,7 @@ class Processor(SoswProcessor):
         self.delete_iam_roles()
 
 
-    def delete_all_lambda_functions(self, region):
+    def delete_all_lambda_functions(self, region: str = "us-west-2"):
         self.lambda_client = boto3.client('lambda', MasterRegion=region)
         response = self.lambda_client.list_functions()
 
@@ -85,10 +86,8 @@ class Processor(SoswProcessor):
             function_name = function['FunctionName']
             if function_name not in self.config['cleanup_exclude_list_lambda']:
                 self.lambda_client.delete_function(FunctionName=function_name)
-                logger.info('deleted lambda function', function_name)
-
+                logger.info('Deleted lambda function: %s', function_name)
                 self.stats['lambdas_deleted'] += 1
-                self.stats['lambdas_deleted'].append(function_name)
 
 
     def delete_users_and_groups(self):
@@ -98,18 +97,17 @@ class Processor(SoswProcessor):
 
         for user in users:
             username = user['UserName']
-            user_groups = self.iam_client.list_groups(UserName=users['UserName'])
-
-            for group in user_groups:
-                self.iam_client.delete_group(GroupName=group)
-                self.stats['groups_deleted'] += 1
-                self.stats['groups_deleted'].append(group)
 
             self.iam_client.delete_user(UserName=username)
             self.iam_client.delete_login_profile(UserName=username)
-            logger.info('Removed user:', username)
+            logger.info('Removed user: %s', username)
             self.stats['users_deleted'] += 1
-            self.stats['users_deleted'].append(username)
+
+        user_groups = self.iam_client.list_groups(UserName=users['UserName'])
+
+        for group in user_groups:
+            self.iam_client.delete_group(GroupName=group)
+            self.stats['groups_deleted'] += 1
 
 
     def delete_customer_managed_policies(self):
@@ -119,31 +117,26 @@ class Processor(SoswProcessor):
 
         for policy in policies_to_die:
             self.iam_client.delete_policy(PolicyName=policy)
-            logger.info("Deleted policy:", policy)
+            logger.info("Deleted policy: %s", policy)
             self.stats['policies_deleted'] += 1
-            self.stats['policies_deleted'].append(policy)
 
 
     def delete_iam_roles(self):
-
         response = self.iam_client.list_roles()
-        roles_to_die = response['Roles']
+        roles = [x for x in response['Roles'] if x not in self.config['cleanup_exclude_list_role']]
 
-        for role in roles_to_die:
+        for role in roles:
             role_name = role['RoleName']
-
-            if role_name != self.config['cleanup_exclude_list_role']:
-                self.iam_client.delete_role(RoleName=role_name)
-                logger.info('Role successfully deleted:', role_name)
-                self.stats['roles_deleted'] += 1
-                self.stats['roles_deleted'].append(role_name)
-            else:
-                logger.info('Role is in exclude list:', role_name)
+            self.iam_client.delete_role(RoleName=role_name)
+            logger.info('Role successfully deleted: %s', role_name)
+            self.stats['roles_deleted'] += 1
+        else:
+            logger.info('No roles to delete or all roles are in exclude list')
 
 
     def generate_random_password(self, length: int = 8):
 
-        characters = string.ascii_letters + string.digits + string.punctuation
+        characters = string.ascii_letters + string.digits + string.punctuation + string.ascii_uppercase
         return ''.join(random.choice(characters) for _ in range(length))
 
 
